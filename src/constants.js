ee9542d2426903a8a191220e5afa5dbd2f836708
Amin <asaa47721@gmail.com>
2020-08-13 20:20:25 +0430

    display products in frontend

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const LOCALHOST = 'http://127.0.0.1:8000'
+
+const API_URL = '/api'
+
+const ENDPOINT = `${LOCALHOST}${API_URL}`
+
+export const PRODUCT_LIST_URL = `${ENDPOINT}/product-list/`
---
+++ src/containers/ProductList.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import {
+    Button,
+    Icon,
+    Image,
+    Item,
+    Label,
+    Container,
+    Segment,
+    Dimmer,
+    Loader,
+    Message
+} from 'semantic-ui-react'
+
+import axios from 'axios'
+
+import { PRODUCT_LIST_URL } from '../constants'
+
+
+class ProductList extends Component {
+
+    state = {
+        loading: false,
+        error: null,
+        date: []
+    }
+
+
+    componentDidMount() {
+        this.setState({
+            loading: true
+        })
+
+        axios.get(PRODUCT_LIST_URL)
+            .then(res => {
+                this.setState({
+                    data: res.data,
+                    loading: false
+                })
+            }).catch(err => {
+                this.setState({
+                    error: err
+                })
+            })
+    }
+
+
+    render() {
+        const { loading, error, data } = this.state
+
+        return (
+            <Container>
+                {error && (
+                    <Message
+                        error
+                        header='There was some errors with your submission'
+                        content={JSON.stringify(error)}
+                    />
+                )}
+
+                {loading && (
+                    <Segment>
+                        <Dimmer active inverted>
+                            <Loader inverted>Loading</Loader>
+                        </Dimmer>
+
+                        <Image src='/images/wireframe/short-paragraph.png' />
+                    </Segment>
+
+                )}
+
+                <Item.Group divided>
+                    {data && data.map(item => (
+                        <Item key={item.slug}>
+                            <Item.Image src={item.image} />
+                            <Item.Content>
+                                <Item.Header as='a'>{item.title}</Item.Header>
+                                <Item.Meta>
+                                    <span className='cinema'>{item.category}</span>
+                                </Item.Meta>
+                                <Item.Description>{item.description}</Item.Description>
+                                <Item.Extra>
+                                    <Button primary floated='right' icon labelPosition="right" >
+                                        Add to card
+                                        <Icon name='cart plus' />
+                                    </Button>
+                                    {item.discount_price && (
+                                        <Label
+                                            color={
+                                                item.label === "primary"
+                                                    ? "blue"
+                                                    : item.label === "secondary"
+                                                        ? "green"
+                                                        : "olive"
+                                            }
+                                        >
+                                            {item.label}
+                                        </Label>
+                                    )}
+                                </Item.Extra>
+                            </Item.Content>
+                        </Item>
+                    ))}
+                </Item.Group>
+            </Container >
+        )
+    }
+}
+
+export default ProductList
