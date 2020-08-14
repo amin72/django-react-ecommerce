f63dd05504a480ceb5bcafd36a811f13240d2392
Amin <asaa47721@gmail.com>
2020-08-14 05:24:25 +0430

    define OrderItemSerializer and OrderSerializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Item
+from core.models import Item, Order, OrderItem
 
 
 class ItemSerializer(serializers.ModelSerializer):
@@ -24,3 +24,29 @@ class ItemSerializer(serializers.ModelSerializer):
 
     def get_label(self, obj):
         return obj.get_label_display()
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item = serializers.StringRelatedField()
+
+    class Meta:
+        model = OrderItem
+        fields = [
+            'id',
+            'item',
+            'quantity'
+        ]
+    
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'order_items'
+        ]
+    
+    def get_order_items(self, obj):
+        return OrderItemSerializer(obj.items.all(), many=True).data
