082729dc322cbd897bde78c530f03cf74a59aec6
Amin <asaa47721@gmail.com>
2020-08-14 05:25:37 +0430

    define OrderDetailAPIView and AddToCartAPIView apis, with urls

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('product-list/', views.ItemListAPIView.as_view(), name='product-list'),
+    path('add-to-cart/', views.AddToCartAPIView.as_view(), name='add-to-cart'),
+    path('order-summary/', views.OrderDetailAPIView.as_view(), name='order-summary'),
 ]
