14da547ad65f0694bebba9bc8f3600ae666fb3c1
Amin <asaa47721@gmail.com>
2020-08-19 03:57:55 +0430

    export base and api urls

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,9 @@
-const LOCALHOST = 'http://localhost:8000'
+export const BASE_URL = 'http://localhost:8000'
+export const API_URL = '/api'
+export const END_POINT = `${BASE_URL}${API_URL}`
 
-const API_URL = '/api'
-
-export const END_POINT = `${LOCALHOST}${API_URL}`
-
-export const PRODUCT_LIST_URL = `${END_POINT}/product-list/`
+export const PRODUCT_LIST_URL = `${END_POINT}/products/`
+export const PRODUCT_DETAIL_URL = id => `${END_POINT}/products/${id}`
 export const ADD_TO_CART_URL = `${END_POINT}/add-to-cart/`
 export const ORDER_SUMNARY_URL = `${END_POINT}/order-summary/`
 export const CHECKOUT_URL = `${END_POINT}/checkout/`
