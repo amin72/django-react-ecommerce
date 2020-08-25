d835623cc4d7d9e39453ee7557ff5d65d683597f
Amin <asaa47721@gmail.com>
2020-08-26 01:29:18 +0430

    load and render addresses and send selected addresses to api

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -11,16 +11,23 @@ import {
     Loader,
     Label,
     Message,
-    Segment
+    Segment,
+    Select
 } from 'semantic-ui-react';
 import { authAxios } from '../utils'
-import { CHECKOUT_URL, ORDER_SUMNARY_URL, ADD_COUPON_URL } from '../constants'
+import {
+    CHECKOUT_URL,
+    ORDER_SUMNARY_URL,
+    ADD_COUPON_URL,
+    ADDRESS_LIST_URL
+} from '../constants'
 import {
     CardElement,
     Elements,
     ElementsConsumer,
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import { Link, withRouter } from 'react-router-dom';
 
 
 class OrderPreview extends Component {
@@ -84,7 +91,10 @@ class CouponForm extends Component {
     handleSubmit = e => {
         const { code } = this.state
         this.props.handleAddCoupon(e, code)
-        this.setState({ code: '' })
+        this.setState({
+            code: '',
+            loading: false
+        })
     }
 
     render() {
@@ -118,11 +128,17 @@ class CheckoutForm extends React.Component {
         error: null,
         success: false,
         stripe: null,
-        data: null
+        data: null,
+        billingAddresses: [],
+        shippingAddresses: [],
+        selectedBillingAddress: '',
+        selectedShippingAddress: ''
     }
 
     componentDidMount() {
-        this.handelFetchOrder()
+        this.handleFetchOrder()
+        this.handleFetchBillingAddresses()
+        this.handleFetchShippingAddresses()
     }
 
     handleSubmit = async (event) => {
@@ -149,7 +165,14 @@ class CheckoutForm extends React.Component {
                     loading: false
                 })
             } else {
-                authAxios.post(CHECKOUT_URL, { stripeToken: result.token.id })
+                this.setState({
+                    error: null
+                })
+                const { selectedBillingAddress, selectedShippingAddress } = this.state
+                authAxios.post(CHECKOUT_URL, {
+                    stripeToken: result.token.id,
+                    selectedBillingAddress, selectedShippingAddress
+                })
                     .then(res => {
                         this.setState({
                             loading: false,
@@ -166,7 +189,7 @@ class CheckoutForm extends React.Component {
         })
     };
 
-    handelFetchOrder = () => {
+    handleFetchOrder = () => {
         this.setState({
             loading: true
         })
@@ -178,11 +201,8 @@ class CheckoutForm extends React.Component {
                     loading: false
                 })
             }).catch(err => {
-                if (err.response.status) {
-                    this.setState({
-                        error: "You currently do not have an order",
-                        loading: false
-                    })
+                if (err.response.status === 404) {
+                    this.props.history.push("/products")
                 } else {
                     this.setState({
                         error: err,
@@ -204,7 +224,7 @@ class CheckoutForm extends React.Component {
                 this.setState({
                     loading: false,
                 })
-                this.handelFetchOrder()
+                this.handleFetchOrder()
             })
             .catch(err => {
                 this.setState({
@@ -214,8 +234,81 @@ class CheckoutForm extends React.Component {
             })
     }
 
+    handleFetchBillingAddresses = () => {
+        this.setState({
+            loading: true
+        })
+
+        const { activeItem } = this.state
+
+        // fetch user addresses
+        authAxios.get(ADDRESS_LIST_URL('B'))
+            .then(res => {
+                this.setState({
+                    billingAddresses: res.data.map(address => {
+                        return {
+                            key: address.id,
+                            text: `${address.street_address}, ${address.apartment_address}, ${address.country}`,
+                            value: address.id
+                        }
+                    }),
+                    selectedBillingAddress: this.handleGetDefaultAddress(res.data),
+                    loading: false
+                })
+            }).catch(err => {
+                this.setState({
+                    error: err,
+                    loading: false
+                })
+            })
+    }
+
+    handleFetchShippingAddresses = async () => {
+        this.setState({
+            loading: true
+        })
+
+        const { activeItem } = this.state
+
+        // fetch user addresses
+        authAxios.get(ADDRESS_LIST_URL('S'))
+            .then(res => {
+                this.setState((state, props) => {
+                    return {
+                        shippingAddresses: res.data.map(address => {
+                            return {
+                                key: address.id,
+                                text: `${address.street_address}, ${address.apartment_address}, ${address.country}`,
+                                value: address.id
+                            }
+                        }),
+                        selectedShippingAddress: this.handleGetDefaultAddress(res.data),
+                        loading: false
+                    }
+                })
+            }).catch(err => {
+                this.setState({
+                    error: err,
+                    loading: false
+                })
+            })
+    }
+
+    handleSelectChange = (e, { name, value }) => {
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleGetDefaultAddress = addresses => {
+        const filteredAddresses = addresses.filter(address => address.default === true)
+        return filteredAddresses.length > 0 ? filteredAddresses[0].id : ''
+    }
+
     render() {
-        const { data, error, loading, success } = this.state
+        const { data, error, loading, success, billingAddresses,
+            shippingAddresses, selectedBillingAddress,
+            selectedShippingAddress } = this.state
 
         return (
             <div>
@@ -235,41 +328,74 @@ class CheckoutForm extends React.Component {
 
                 )}
 
-                {success && (
-                    <Message positive>
-                        <Message.Header>Your payment was successful</Message.Header>
-                        <p>
-                            Go to your <b>profile</b> to see your order delivery status.
-                      </p>
-                    </Message>
-                )}
-
                 <OrderPreview data={data} />
                 <Divider />
                 <CouponForm handleAddCoupon={(e, code) => this.handleAddCoupon(e, code)} />
                 <Divider />
 
-                <Header>Would you like to complete the purchase?</Header>
-                <Form onSubmit={this.handleSubmit}>
-                    <CardElement />
-                    <Button
-                        primary
-                        loading={loading}
-                        disabled={loading}
-                        type="submit"
-                        style={{ marginTop: '20px' }}
-                    >
-                        Pay
-                    </Button>
+                <Header>Select a billing address</Header>
+                {billingAddresses.length > 0 ? (
+                    <Select
+                        fluid
+                        clearable
+                        selection
+                        name='selectedBillingAddress'
+                        value={selectedBillingAddress}
+                        options={billingAddresses}
+                        onChange={this.handleSelectChange}
+                    />) : (
+                        <p>You need to <Link to="/profile" >add a billing addresses</Link></p>
+                    )}
 
-                </Form>
+                <Header>Select a shipping address</Header>
+                {shippingAddresses.length > 0 ? (
+                    <Select
+                        fluid
+                        clearable
+                        selection
+                        name='selectedShippingAddress'
+                        value={selectedShippingAddress}
+                        options={shippingAddresses}
+                        onChange={this.handleSelectChange}
+                    />) : (
+                        <p>You need to <Link to="/profile" >add a shipping addresses</Link></p>
+                    )}
+
+                {billingAddresses.length < 1 || shippingAddresses.length < 1 ? (
+                    <p>You need to add addresses before you can complete your purchage</p>
+                ) : (
+                        <Fragment>
+                            <Header>Would you like to complete the purchase?</Header>
+                            <Form onSubmit={this.handleSubmit}>
+                                <CardElement />
+                                {success && (
+                                    <Message positive>
+                                        <Message.Header>Your payment was successful</Message.Header>
+                                        <p>
+                                            Go to your <b>profile</b> to see your order delivery status.
+                                        </p>
+                                    </Message>
+                                )}
+                                <Button
+                                    primary
+                                    loading={loading}
+                                    disabled={loading}
+                                    type="submit"
+                                    style={{ marginTop: '20px' }}
+                                >
+                                    Pay
+                            </Button>
+                            </Form>
+                        </Fragment>
+                    )}
             </div>
         );
     }
 }
 
 
-const stripePromise = loadStripe('pk_test_q5g16dlUVvSaKuIkx9CzjdnI004U0kcgIM');
+const CheckoutFormWithRouter = withRouter(CheckoutForm)
+const stripePromise = loadStripe('pk_test_q5g16dlUVvSaKuIkx9CzjdnI004U0kcgIM')
 
 const Checkout = () => (
     <Elements stripe={stripePromise}>
@@ -278,7 +404,7 @@ const Checkout = () => (
                 <Header as="h1">Complete your order</Header>
                 <ElementsConsumer>
                     {({ elements, stripe }) => (
-                        <CheckoutForm elements={elements} stripe={stripe} />
+                        <CheckoutFormWithRouter elements={elements} stripe={stripe} />
                     )}
                 </ElementsConsumer>
             </div>
